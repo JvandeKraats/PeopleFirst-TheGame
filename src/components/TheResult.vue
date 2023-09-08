1ba3b2870b69676690294e76fd01de611d4c698a
Jeroen van de Kraats <jvandekraats@xpirit.com>
2023-09-08 21:24:51 +0200

    Added try again button

--- src/components/TheResult.vue
+++ src/components/TheResult.vue
@@ -1,38 +1,61 @@
 <template>
-    <div class="score-container">
-      <h1 :class="score >= 5 ? 'high-score' : 'low-score'">Your Score: {{ score }}</h1>
-    </div>
-  </template>
+  <div class="score-container">
+    <h1 :class="score >= 5 ? 'high-score' : 'low-score'">Your Score: {{ score }}</h1>
+    <button @click="resetScore" class="try-again-button">Try Again</button>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        score: null
-      };
-    },
-    mounted() {
-      const storedScore = JSON.parse(localStorage.getItem('gameScore'));
-      if (storedScore !== null) {
-        this.score = storedScore.toFixed(1);
-      }
+<script>
+export default {
+  data() {
+    return {
+      score: null
+    };
+  },
+  mounted() {
+    const storedScore = JSON.parse(localStorage.getItem('gameScore'));
+    if (storedScore !== null) {
+      this.score = storedScore.toFixed(1);
     }
-  };
-  </script>
+  },
+  methods: {
+    resetScore() {
+      this.$router.push('/');
+    }
+  }
+};
+</script>
   
-  <style>
-  .score-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    font-size: 2em;
-  }
-  .high-score {
-    color: green;
-  }
-  .low-score {
-    color: red;
-  }
-  </style>
+<style>
+.score-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2em;
+}
+
+.high-score {
+  color: green;
+}
+
+.low-score {
+  color: red;
+}
+
+.try-again-button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.try-again-button:hover {
+  background-color: #0056b3;
+}
+</style>
   
