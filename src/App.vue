8902dda01fcbacea53cd6400b7015aaa32c67ab9
mstepanova-xpirit <mstepanova@xpirit.com>
2023-09-08 17:58:17 +0200

    rename views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeHeader from './components/WelcomeHeader.vue'
 </script>
 
 <template>
@@ -8,12 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <WelcomeHeader msg="Welcome to the game!" />
     </div>
   </header>
 
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,7 +1,11 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <RouterLink to="/game">Start the game</RouterLink>
 </template>
 
 <style>
