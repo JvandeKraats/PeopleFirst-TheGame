e06640f2861f922ddbaf1584ff0ca46150533d4d
mstepanova-xpirit <mstepanova@xpirit.com>
2023-09-08 19:28:18 +0200

    add some contetnt to component

--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -1,5 +1,31 @@
-<script setup>
-
+<script>
+export default {
+  props: {
+    collegas: Array, // Define a prop for the list
+  },
+  data() {
+    return {
+      currentIndex: 0, // Start with the first item
+    };
+  },
+  methods: {
+    nextItem() {
+      if (this.currentIndex < this.collegas.length - 1) {
+        this.currentIndex++;
+      }
+    },
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div>
+    <p>Guess who is that: {{ collegas[currentIndex].Name }}</p>
+    <p v-if="currentIndex < collegas.length - 1">
+      <button @click="nextItem">Next</button>
+    </p>
+    <p v-else>
+      <em>No more items</em>
+    </p>
+  </div>
+</template>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,7 +1,31 @@
-<script setup>
+<script>
 import TheGame from '../components/TheGame.vue'
+
+export default {
+  components: {
+    TheGame, // Register the TheGame component
+  },
+  data() {
+    return {
+      myList: [
+        {
+          "Name": "Patrick",
+          "Image": "https://xpirit.com/wp-content/uploads/2021/11/patrick-de-kruijff.png"
+        },
+        {
+          "Name": "Natascha",
+          "Image": "https://xpirit.com/wp-content/uploads/2020/12/natascha-former.png"
+        },
+        {
+          "Name": "Jasper",
+          "Image": "https://xpirit.com/wp-content/uploads/2020/12/jasper-gilhuis.jpg"
+        }
+      ], // Your list of items
+    };
+  },
+};
 </script>
 
 <template>
-  <TheGame />
+  <TheGame :collegas="myList" />
 </template>
