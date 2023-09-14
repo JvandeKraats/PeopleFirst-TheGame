ca114c8ce788131376bfdb1fa2f7f5ce34deb8d4
Jeroen <J_v_d_K@hotmail.com>
2023-09-14 21:09:03 +0200

    Added app service plan + linux web app (#4)
    Co-authored-by: Jeroen van de Kraats <jvandekraats@xpirit.com>

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,3 +2,24 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-PeopleFirst-Test"
   location = "westeurope"
 }
+
+resource "azurerm_service_plan" "sp" {
+  name                = "PeopleFirst-Plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "lwa" {
+  name                = "PeopleFirst-WebApp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
+}
