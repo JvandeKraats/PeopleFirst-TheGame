cf194f9115ad0c811da44ee85380659ceb013408
Jeroen van de Kraats <jvandekraats@xpirit.com>
2023-09-14 20:14:16 +0200

    Added first pipeline and terraform code

---
+++ Infra/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.5.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.72.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "statefiles"
+    storage_account_name = "alsdfjweroqokdkdlll"
+    container_name       = "statefile"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
